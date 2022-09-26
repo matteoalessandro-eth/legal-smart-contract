4319a89716c1bc27986fdc2a301155cf5f36eb83
matteoalessandro-eth <matteo@matteoalessandro.com>
2022-09-26 07:54:14 -0400

    adding initial smart contract

--- bill-of-exchange/README.md
+++ bill-of-exchange/README.md
@@ -1,8 +1,26 @@
 ### Smart Contracts used for Bills of Exchange
 
-The first implementation which will be considered is the use of smart contracts to create bills of exchange. These will be analysed in the local (Maltese) context, and then a basic implementation of them is proposed. 
+The first implementation which will be considered is the use of smart contracts to create bills of exchange ("BoE"). These will be analysed in the local (Maltese) context, and then a basic implementation of them is proposed. 
 
 #### Assumptions
 1. That tokens are construed as 'containers' which can hold rights, as was implemented by the Liechtenstein TVTG Act and as proposed by myself in a forthcoming paper.
 
 2. That methods for identity verification are available, and accepted legally.
+
+#### Proposed Method
+1. Two parties who come to an agreement deploy and sign a smart contract which issues an NFT (or as many as necessary) to represent the obligations. The NFT is sent to the party who is due to receive the amount.
+
+2. The party promising to pay the amount can also choose to have the payments made automatically.
+
+3. The NFT is burnt once payment has been accepted by the receiver, thus terminating the obligation.
+
+
+#### NFT Data Needed
+1. Identification of both parties
+
+2. Information about the BoE including:
+    a. Amount
+    b. Payment date
+    c. Jurisdiction
+
+3. A URI to the natural language version of the BoE stored on IPFS
--- bill-of-exchange/contracts/boe.sol
+++ bill-of-exchange/contracts/boe.sol
@@ -6,5 +6,97 @@ pragma solidity ^0.8.0;
 *
 *   @title Bills of Exchange Smart Contract
 *   @dev Implement bills of exchange through a smart contract which issues a token representative of the right
+*   
+*   NOTE: This is purely a proof of concept, it is not to be used in real-life transactions as it has not been tested properly.   
 *
+*   Made use of:
+*   BillOfSale.sol by Michael Rice (https://github.com/mrice/solidity-legal-contracts)
+*   DateUtils by Skeleton Codeworks (http://github.com/SkeletonCodeworks/DateUtils)
 */
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./Date.sol";
+
+
+contract BillOfExchange is Ownable {
+
+    using SafeMath for uint256;
+
+    address public promisee;
+    address public promisor;
+    string public promiseeName;
+    string public promisorName;
+    uint public billAmount;
+    string public billDescription;
+    bool public promiseeConsent = false;
+    bool public promisorConsent = false;
+    bool public billPaid = false;
+    uint public dateOfEntry;
+    uint public dateOfExpiry;
+    string public naturalLanguage;
+
+
+    // initial constructor setting basic information needed
+    constructor(address _promisee, address _promisor, string memory _promiseeName, string memory _promisorName) public {
+        promisee = _promisee;
+        promisor = _promisor;
+        promiseeName = _promiseeName;
+        promisorName = _promisorName;
+    }
+
+    // sets the amount for the BoE
+    function setBillAmount(uint _billAmount) public promiseeOnly {
+        billAmount = _billAmount;
+    }
+
+    // sets the billDescription;
+    function setBillDescription(string memory _billDescription) public promiseeOnly {
+        billDescription = _billDescription;
+    }
+
+    // determines party consent
+
+    function setPromiseeConsent() public promiseeOnly {
+        promiseeConsent = true;
+    }
+
+    function setPromisorConsent() public promisorOnly {
+        promisorConsent = true;
+    }
+
+    // sets date of expiry
+
+    function setDateOfExpiry(string memory _dateOfExpiry){
+        dateOfExpiry = Date.setDateFromString(_dateOfExpiry);
+    }
+
+    // modifiers
+
+    modifier promisorOnly(){
+        require(msg.sender == promisor, "only the promisor can send this message");
+        _;
+    }
+
+    modifier promiseeOnly(){
+        require(msg.sender == promisee, "only the promisee can send this message");
+        _;
+    }
+
+    modifier partiesOnly(){
+        require(msg.sender == promisor || msg.sender == promisee, "only parties to this agreement can send this message");
+        _;
+    }
+
+    modifier needConsent(){
+        require(promiseeConsent == true && promisorConsent == true);
+        _;
+    }
+
+
+
+}
